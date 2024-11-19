9c60d3a72f7499b79eb5508f5b2f4749aa0bcfae
Lionel Domguia <lionel.domguia@gmail.com>
2024-11-19 13:07:47 -0500

    Task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,30 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
+const isValid = (username) => { //returns boolean
 //write code to check is the username is valid
+    return users.find(u => u.username === username);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+const authenticatedUser = (username, password) => { //returns boolean
 //write code to check if username and password match the one we have in records.
+    return users.find(u => u.username === username && u.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+	console.log('LOGIN')
+  const { username, password } = req.body;
+
+  if(isValid(username) && authenticatedUser(username, password)){
+		// Add user credentials to session as jwt
+    req.session.user = jwt.sign(username,  "secret-key");
+
+    return res.status(200).json("Logged in successfully!");
+  }
+
+	return res.status(400).json("Invalid credentials");
 });
 
 // Add a book review
